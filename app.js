bf1c324bfd13007635c2eb51157f63c514f3fb6b
Manuel <mninahuanca4@gmail.com>
2021-05-08 23:16:25 -0400

    Servir un sitio web completo

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ const app = express();
 // Servir Contenido estatico
 app.use(express.static('public'));
 
-app.get('/hola-mundo',  (req, res) => {
-    res.send('Hola Mundo desde su respectiva pagina');
-})
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html');
+});
+
+app.get('/Home', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html');
+});
 
 app.get('*',  (req, res) => {
     res.sendFile(__dirname + '/public/404.html');
